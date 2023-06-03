c2385d716e8121d6e7462f41743bf0fb20f99be8
Dan1Almeida <daniel.dnlsilva7@gmail.com>
2023-06-02 22:21:17 -0300

    Fim de Aula Sem Alterações

---
+++ aforca.py
@@ -0,0 +1,11 @@
+def gg():
+    print('**************************************')
+    print('---------- Jogo da Forca -------------')
+    print('**************************************')
+
+    print('GG Forca')
+    print('\nObrigado por participar\n')
+
+
+if __name__ == '__main__':
+    gg()
